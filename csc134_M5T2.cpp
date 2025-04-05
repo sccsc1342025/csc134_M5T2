36b4c3483f2795708f5e666d01df59ff97ba8755
sccsc1342025 <sebastiancam711@gmail.com>
2025-04-05 16:46:11 +0000

    csc134_M5T2.cpp

--- csc134_M5T2.cpp
+++ csc134_M5T2.cpp
@@ -6,9 +6,29 @@
 #include <iostream>
 using namespace std;
 
+void printResult(int number, int result)
+{
+    cout << number << " squared= " << result << endl;
+}
+
+int square(int number)
+{
+    int result;
+    result = number * number;
+    return result;
+}
+
 int main() 
 {
-   
+   int count = 1;
+   int result;
+
+   while (count <= 10)
+   {
+    result = square(count);
+    printResult(count, result);
+    count++;
+   }
 
    return 0;
 }
